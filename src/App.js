8a8cf4d037336ee540737bac9c588ae9a6b46765
TreeTD <t.dudek@tree.com.pl>
2024-03-12 11:48:09 +0100

    template-forms-tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form>
+        <div className="form-group">
+          <input className="form-field" placeholder="Imię" name="firstName" />
+          <input
+            className="form-field"
+            placeholder="Nazwisko"
+            name="lastName"
+          />
+          <input className="form-field" placeholder="Email" name="email" />
+        </div>
+      </form>
     </div>
   );
 }
